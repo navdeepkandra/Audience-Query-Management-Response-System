93e38accc2295f0dbbd8397d1977e648b50c03ba
Navdeep C <navdeepkandra123@gmail.com>
2025-11-16 11:28:54 +0530

    Fixed internal hostname

--- Backend/routes/queryRoutes.js
+++ Backend/routes/queryRoutes.js
@@ -4,7 +4,7 @@ const Query = require('../models/Query');
 const axios = require('axios');
 const mongoose = require('mongoose');
 
-const CLASSIFICATION_API = 'http://nlp-classifier.onrender.com/classify'; 
+const CLASSIFICATION_API = 'http://nlp-classifier:5001/classify'; 
 
 
 router.get('/', async (req, res) => {
